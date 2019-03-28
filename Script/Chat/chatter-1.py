a81b4000aacda836d03a3fde20bd75421aa81262
VIGER-Valentin <vigervalentin@gmail.com>
2019-03-28 09:54:33 +0100

    un fichier txt pour les stopper, et des les ténèbres les lier

--- Script/Chat/chatter-1.py
+++ Script/Chat/chatter-1.py
@@ -5,6 +5,7 @@ import random
 
 sentences = ["Meet me at the coffe machine","Wow the last company party was so lit","Hey is it right you hooked up with the boss wife ?","Send you a mail with my new meme","Did you watch the game last night ?","Lets grab lunch","Thanks God its Friday"]
 
+#set up the server socket
 sock = socket(AF_INET, SOCK_STREAM)
 sock.settimeout(60)
 try :
@@ -14,25 +15,30 @@ except Exception as e:
 
 sock.listen(3)
 
+#keep running, but do nothing if the flag file is at 0
 while True:
-    try: 
-        print("---")
-        connexion, address = sock.accept()
-        print("connected from {}".format(address))
-        f = connexion.makefile('rw', buffering=None)
-        toSend = sentences[random.randint(0,6)]
-        print(toSend)
-        f.write(toSend)
-        f.flush()
-        data = f.readline()
-        if data:
-            print(data)
-        else:
+    frun = open('DoNotTouch.txt','r')
+    run = frun.read()
+    if run == '1':
+        try: 
+            print("---")
+            #wait a new connection
+            connexion, address = sock.accept()
+            print("connected from {}".format(address))
+            #open socket as a file
+            f = connexion.makefile('rw', buffering=None)
+            toSend = sentences[random.randint(0,6)]
+            print(toSend)
+            #send his random message then wait the answer
+            f.write(toSend)
+            f.flush()
+            data = f.readline()
+            #close the socket
             f.close()
             connexion.close()
-        f.close()
-        connexion.close()
-    except Exception as e:
-        pass
+        except Exception as e:
+            pass
+        
+    frun.close()
     
 sock.close()
--- Script/SMTP/smtpFreak.py
+++ Script/SMTP/smtpFreak.py
@@ -11,50 +11,55 @@ password = ''
 seenMails = []
 
 while True:
-    #retrieve mails already seen from the picke
-    if os.stat("seenMails.txt").st_size != 0 :
-        with open ('seenMails.txt', 'rb') as fp:
-            seenMails = pickle.load(fp)
+    #check the flag file to check the emails or not
+    frun = open('DoNotTouch.txt','r')
+    run = frun.read()
+    if run == '1':
+        #retrieve mails already seen from the picke
+        if os.stat("seenMails.txt").st_size != 0 :
+            with open ('seenMails.txt', 'rb') as fp:
+                seenMails = pickle.load(fp)
 
-    imapper = easyimap.connect('outlook.office365.com', login, password)
-    
-    #retrieve the 3 last mails
-    for mail_id in imapper.listids(limit=3):
-        mail = imapper.mail(mail_id)
-        if (mail.title, mail.date) not in seenMails:
-            seenMails.append((mail.title, mail.date))
-            print(mail.from_addr)
-            print()
-            print(mail.to)
-            print()
-            print(mail.title)
-            print()
-            
-            toExec = []
-            try:
-                #for each attachement in every mail : load then execute
-                for attachment in mail.attachments:
-                    if '?iso-8859-1?' in attachment[0]:
-                        temp = decode_header(attachment[0])[0]
-                        f = open("attachments/" + temp[0].decode('utf-8'), "wb")
-                        f.write(attachment[1])
-                        f.close()
-                        toExec.append(temp[0])
-                    else:
-                        f = open("attachments/" + attachment[0], "wb")
-                        f.write(attachment[1])
-                        f.close()
-                        toExec.append(attachment[0])
-                for f in toExec:
-                    print(f)
-                    os.system('explorer.exe "attachments\\'+f+'"')
-            except Exception as e:
-                print(str(e))
-                pass
-    
-    #put every seen mails into the pickle file
-    with open('seenMails.txt', 'wb') as fp:
-        pickle.dump(seenMails, fp)
-                
-    print('waiting next iteration ------')
-    time.sleep(120)
+        imapper = easyimap.connect('outlook.office365.com', login, password)
+
+        #retrieve the 3 last mails
+        for mail_id in imapper.listids(limit=3):
+            mail = imapper.mail(mail_id)
+            if (mail.title, mail.date) not in seenMails:
+                seenMails.append((mail.title, mail.date))
+                print(mail.from_addr)
+                print()
+                print(mail.to)
+                print()
+                print(mail.title)
+                print()
+
+                toExec = []
+                try:
+                    #for each attachement in every mail : load then execute
+                    for attachment in mail.attachments:
+                        if '?iso-8859-1?' in attachment[0]:
+                            temp = decode_header(attachment[0])[0]
+                            f = open("attachments/" + temp[0].decode('utf-8'), "wb")
+                            f.write(attachment[1])
+                            f.close()
+                            toExec.append(temp[0])
+                        else:
+                            f = open("attachments/" + attachment[0], "wb")
+                            f.write(attachment[1])
+                            f.close()
+                            toExec.append(attachment[0])
+                    for f in toExec:
+                        print(f)
+                        os.system('explorer.exe "attachments\\'+f+'"')
+                except Exception as e:
+                    print(str(e))
+                    pass
+
+        #put every seen mails into the pickle file
+        with open('seenMails.txt', 'wb') as fp:
+            pickle.dump(seenMails, fp)
+
+        print('waiting next iteration ------')
+        time.sleep(120)
+    frun.close()
