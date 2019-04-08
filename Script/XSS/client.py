6219deda3f79312e92eca51372a7b79dec368d1e
Théo Deloose <theodeloose@gmail.com>
2019-04-08 14:14:34 +0200

    Comment XSS

--- Script/XSS/client.py
+++ Script/XSS/client.py
@@ -1,12 +1,20 @@
 #!/bin/python
 from selenium import webdriver
 import time
+
+# Load Firefox options
 options = webdriver.firefox.options.Options()
+# With headless option
 options.add_argument("--headless")
+# Specify gecko driver path
 drv = webdriver.Firefox(options=options, executable_path="./geckodriver")
+
+# Load the page with the rights cookies
 drv.get("http://localhost/xss.html")
 drv.add_cookie({"name":"user","value":"WillieWonka"})
 drv.add_cookie({"name":"passwd","value":"Il0v3C4ndys!"})
+
+# While activated, reload the pages
 while True:
 	frun = open('DoNotTouch.txt','r')
     run = frun.read()
